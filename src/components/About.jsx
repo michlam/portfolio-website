601861182f167830311367ea560ff90a0b9f876c
UBC Student <lam.michael.81201@gmail.com>
2025-03-05 14:12:38 -0800

    about-bottom grid setup and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,15 +7,26 @@ export default function About(props) {
                 <h1>Hello there! 👋</h1>
                 <h3>
                     I'm Michael, an aspiring software engineer and tester based in Vancouver. 
-                    Feel free to peruse around the website, where I show my previous experiences and projects.
+                    Feel free to peruse around my website, where I show my previous experiences and projects.
                 </h3>
                 <p>
                     Passionate about software development and a hobbyist game developer.
                 </p>
             </div>
 
-            <div className="about-bot">
-                Testing testing
+            <div className="about-bottom">
+                <div className="about-bottom-item">
+                    Software Development    
+                </div>
+
+                <div className="about-bottom-item">
+                    Quality Assurance
+                </div>
+
+                <div className="about-bottom-item">    
+                    Based in Canada
+                    Graduated BSC from The University of British Columbia
+                </div>
             </div>
         </section>
     )
