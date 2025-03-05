5e1b0ae666de31d2316ea9e7b8ac9cb857cc8585
UBC Student <lam.michael.81201@gmail.com>
2025-03-04 21:14:17 -0800

    Home page name and title, styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home(props) {
     return (
         <section className="home">
             <div className="home-content">
-                <h1>Michael Lam</h1>
+                <h1>MICHAEL LAM</h1>
                 <h2>Software Developer / Quality Assurance</h2>
             </div>
         </section>
