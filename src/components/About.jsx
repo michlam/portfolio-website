3a49b3acdf3e9280d758208d2b34633f0a6b4fad
UBC Student <lam.michael.81201@gmail.com>
2025-03-05 13:52:58 -0800

    about-top styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About(props) {
     return (
         <section className="about">
             <div className="about-top">
-                <h1>Hello there, and welcome! 👋</h1>
+                <h1>Hello there! 👋</h1>
                 <h3>
                     I'm Michael, an aspiring software engineer and tester based in Vancouver. 
                     Feel free to peruse around the website, where I show my previous experiences and projects.
@@ -12,6 +12,10 @@ export default function About(props) {
                 <p>
                     Passionate about software development and a hobbyist game developer.
                 </p>
+            </div>
+
+            <div className="about-bot">
+                Testing testing
             </div>
         </section>
     )
