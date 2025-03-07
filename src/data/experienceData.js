2c7369aa6f74f79df5d2d7afc439a613d584fc27
UBC Student <lam.michael.81201@gmail.com>
2025-03-06 17:18:04 -0800

    finished gravit-e experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,7 +3,7 @@ export default [
         date1: "Jan - Aug",
         date2: "2024",
         role: "QA Analyst Intern @ Vecima Networks Inc.",
-        location: "Vancouver, BC",
+        location: "Burnaby, BC",
 
         link: "https://neroglobal.com/",
         description: "During this internship, I worked to test every part of the GPS solution suite, including websites, internal tools, APIs, third-party integrations, and hardware.",
@@ -14,20 +14,43 @@ export default [
             "As principal QA for a third party integration, translated client requirements into actionable E2E test plans and automated tests within a strict release schedule.",
         ],
         skills: [
-            "JavaScript", "Sahi", "GPS", "Test Automation", "Agile",
+            "JavaScript", "Sahi", "GPS", "Test Automation", "Quality Assurance", "Agile",
         ],
         imagePath: "/Vecima_Logo.svg"
     },
     {
         date1: "Jan - Aug",
-        date2: "2024",
-        role: "QA Analyst Intern @ Vecima Networks 2",
+        date2: "2022",
+        role: "QA Tester Intern @ Gravit-e Technologies",
         location: "Vancouver, BC",
 
-        link: "https://neroglobal.com/",
-        description: "During this internship, I worked to test every part of the GPS solution suite, including websites, internal tools, APIs, third-party integrations, and hardware.",
+        link: "https://www.gravit-e.ca/",
+        description: "As part of the QA team, I tested over thirty client websites, learning about the software development lifecycle, collaboration with other QA and developers, and translating client requirements into reusable test cases.",
+        
         responsibilities: [
-            "Developed and executed functional tests for over seven GPS device models, adhering to criteria set by client use cases and specific device protocols.",
+            "Developed unit and integration tests to verify expected behaviours for new features and to prepare for future regression tests.",
+            "Assisted in the migration to TestRail by building up a reusable repository of organized and well documented test plans, achieving significant time savings in test case creation.",
+            "Utilized SQL to query the database for expected testing data and investigate bugs that show up in the UI.",
+            "Collaborated with UI/UX developers to propose enhancements to website functionality beyond client requirements.",
+            "Assisted with the onboarding of a new QA member by providing frequent guidance and helping to build a strong rapport.",
+        ],
+        skills: [
+            "SQL", "Postman", "TestRail", "Quality Assurance", "Agile"
+        ],
+        imagePath: "/Gravite_Logo.svg"
+    },
+    {
+        date1: "Jan - Aug",
+        date2: "2022",
+        role: "QA Tester Intern @ Gravit-e Technologies3",
+        location: "Vancouver, BC",
+
+        link: "https://www.gravit-e.ca/",
+        description: "As part of the QA team, I tested over thirty client websites, learning about the software development lifecycle, collaborating with other QA and developers, and translating client requirements into reusable test cases.",
+        
+        
+        responsibilities: [
+            "Developed unit and integration tests to verify expected behaviours for new features and to prepare for future regression tests.",
             "Contributed over 100 test cases to our Sahi test automation suite, significantly reducing release night downtime.",
             "Created a robust and scalable automation script to initialize hundreds of devices, saving hours of manual input for bulk client registration.",
             "As principal QA for a third party integration, translated client requirements into actionable E2E test plans and automated tests within a strict release schedule.",
