dbab32b7079c3d9a648eb4fafdacef296a2f9e39
UBC Student <lam.michael.81201@gmail.com>
2025-03-07 16:13:32 -0800

    added first three thumbnail images

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,15 +4,43 @@ export default [
         thumbnailPath: "/projects/genshin-loadouts/Landing.png",
     },
     {
-        name: "Genshin Loadouts2",
+        name: "Frontend Mentor",
+        thumbnailPath: "/projects/frontend-mentor/thumbnail.png",
+    },
+    {
+        name: "Plungerman",
+        thumbnailPath: "/projects/plungerman/thumbnail.png",
+    },
+    {
+        name: "Exam Day",
         thumbnailPath: "/projects/genshin-loadouts/Landing.png",
     },
     {
-        name: "Genshin Loadouts3",
+        name: "Swarm",
         thumbnailPath: "/projects/genshin-loadouts/Landing.png",
     },
     {
-        name: "Genshin Loadouts4",
+        name: "OpenALPR Image Processing Pipeline",
+        thumbnailPath: "/projects/genshin-loadouts/Landing.png",
+    },
+    {
+        name: "Dungeons Inc.",
+        thumbnailPath: "/projects/genshin-loadouts/Landing.png",
+    },
+    {
+        name: "UBC Notes",
+        thumbnailPath: "/projects/genshin-loadouts/Landing.png",
+    },
+    {
+        name: "COVID Vaccine Registration Simulator",
+        thumbnailPath: "/projects/genshin-loadouts/Landing.png",
+    },
+    {
+        name: "Calendar Task Tracker",
+        thumbnailPath: "/projects/genshin-loadouts/Landing.png",
+    },
+    {
+        name: "Youth Financial Tracking App",
         thumbnailPath: "/projects/genshin-loadouts/Landing.png",
     }
 ]
