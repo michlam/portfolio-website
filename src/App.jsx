2e4f278fb328b956615528f8b9411e3f48f32936
UBC Student <lam.michael.81201@gmail.com>
2025-03-09 14:40:01 -0700

    started contact structuring

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About/>
       <Experience/>
       <Projects/>
+      <Contact />
     </>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import "./Contact.css";
+
+export default function Contact() {
+    return (
+        <footer id="contact" className="contact">
+            <div className="connect-email">
+                <h2>LET'S CONNECT!</h2>
+                <h1>lam.michael.81201@gmail.com</h1>
+            </div>
+
+            <div className="connect-links">
+                <h4>CONNECT</h4>
+                <a><p>LinkedIn</p></a>
+                <a><p>GitHub</p></a>
+            </div>
+        </footer>
+    )
+}
